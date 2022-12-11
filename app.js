5ef60173dc95bbda9e5d47a0367e5aafadd92713
reloaded22 <andres.rojas.talent@gmail.com>
2022-12-11 11:27:17 -0500

    Change root route from '/' to '/test'

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", testRouter);
+app.use("/test", testRouter);
 
 const PORT = process.env.PORT || 3000;
 
